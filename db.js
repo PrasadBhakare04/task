88d303b6444e7e380807dc184dfccdb9c5802eea
Prasad Bhakare <153743967+PrasadBhakare04@users.noreply.github.com>
2024-11-21 15:39:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Prasad@2004',
-    database: 'task',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
 db.connect(function (err) {
